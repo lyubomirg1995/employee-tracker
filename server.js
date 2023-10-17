da4fc6140e994edec92439a61b2531d6a10432b8
lyubomirg1995 <lyubomirg17@gmail.com>
2023-10-17 15:48:27 -0400

    add: dependencies, connect to database, and starting prompts

---
+++ server.js
@@ -0,0 +1,72 @@
+
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Bulgariaisgreat6!",
+        database: "employees_db",
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+db.connect(function (err) {
+    if (err) throw err;
+    console.log("*************************************");
+    console.log("           EMPLOYEE TRACKER          ");
+    console.log("*************************************");
+    startingQuestion();
+});
+
+
+function startingQuestion() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'intro',
+            message: 'What would you like to do?',
+            choices:[
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit'
+            ]
+        }
+    ]).then((answer) => {
+        switch(answer.intro) {
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateRole();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Quit':
+                console.log('Good-Bye!');
+                db.end();
+                break;
+        }
+    });
+};
